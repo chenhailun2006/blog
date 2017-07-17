8f1868ee58ad508a462c23d2801f8fb7cc947e6e
allen <83970162@qq.com>
2017-07-17 21:58:55 +0800

    add components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article/article.component';
+import { DiaryComponent } from './diary/diary.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { DiaryListComponent } from './diary-list/diary-list.component';
+import { CommentComponent } from './comment/comment.component';
+import { CommentListComponent } from './comment-list/comment-list.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ArticleComponent],
+  declarations: [ArticleComponent, DiaryComponent, HeaderComponent, FooterComponent, ArticleListComponent, DiaryListComponent, CommentComponent, CommentListComponent],
   exports: [
     ArticleComponent
   ]
---
+++ src/app/components/diary-list/diary-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-diary-list',
+  templateUrl: './diary-list.component.html',
+  styleUrls: ['./diary-list.component.scss']
+})
+export class DiaryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
