3375006b54a98d4f930bb2742ee1a867a4f317f5
Allen Chen <allen.h.chen@pwc.com>
2017-07-17 18:21:31 +0800

    add article component

---
+++ src/app/components/article/article.component.ts
@@ -0,0 +1,16 @@
+import { Article } from '../../models/article';
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
