9c13e0930b16a8a13ef37a947a0584f71a6e738f
Allen Chen <allen.h.chen@pwc.com>
2017-07-18 17:02:02 +0800

    add search component jumbotron component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,20 +2,47 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleComponent } from './article/article.component';
 import { DiaryComponent } from './diary/diary.component';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { DiaryListComponent } from './diary-list/diary-list.component';
 import { CommentComponent } from './comment/comment.component';
 import { CommentListComponent } from './comment-list/comment-list.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { RankComponent } from './rank/rank.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
-  declarations: [ArticleComponent, DiaryComponent, HeaderComponent, FooterComponent, ArticleListComponent, DiaryListComponent, CommentComponent, CommentListComponent],
+  declarations: [
+    ArticleComponent,
+    DiaryComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleListComponent,
+    DiaryListComponent,
+    CommentComponent,
+    CommentListComponent,
+    JumbotronComponent,
+    RankComponent,
+    SearchComponent
+  ],
   exports: [
-    ArticleComponent
+    ArticleComponent,
+    DiaryComponent,
+    HeaderComponent,
+    FooterComponent,
+    ArticleListComponent,
+    DiaryListComponent,
+    CommentComponent,
+    CommentListComponent,
+    JumbotronComponent,
+    RankComponent,
+    SearchComponent
   ]
 })
 export class ComponentsModule { }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  navbar = [
+    {title: '首页', href: '#'},
+    {title: '开发总结', href: '#'},
+    {title: '技术文章', href: '#'},
+    {title: '每日心语', href: '#'},
+    {title: '关于我', href: '#'}
+  ];
+  currentIndex = 0;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  navbarClicked(event, nav, index) {
+    event.preventDefault();
+    this.currentIndex = index;
+    console.log(event, nav)
   }
 
 }
